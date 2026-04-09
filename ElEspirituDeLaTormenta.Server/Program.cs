e3fa1832c992f8a46cda18a293387d260590299e
Gustavo Narancio <eze_tavo@hotmail.com>
2026-04-09 01:39:26 -0300

    dale que se me va nico a dormir

--- ElEspirituDeLaTormenta.Server/Program.cs
+++ ElEspirituDeLaTormenta.Server/Program.cs
@@ -1,47 +1,54 @@
-using ElEspirituDeLaTormenta.Server.Models;
-using Microsoft.EntityFrameworkCore;
+        using ElEspirituDeLaTormenta.Server.Models;
+        using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ElEspirituDeLaTormentaDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services.AddDbContext<ElEspirituDeLaTormentaDbContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-// Configuración de CORS
-// Configuración de CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("PermitirVercel",
-        policy =>
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        // Configuración de CORS
+        // Configuración de CORS
+        builder.Services.AddCors(options =>
         {
-            policy.AllowAnyOrigin()  // <-- ESTA ES LA MAGIA: Deja entrar a cualquier URL
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
+            options.AddPolicy("PermitirVercel",
+                policy =>
+                {
+                    policy.AllowAnyOrigin()  // <-- ESTA ES LA MAGIA: Deja entrar a cualquier URL
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
         });
-});
 
-var app = builder.Build();
+        var app = builder.Build();
+        // --- INICIO DE CÓDIGO NUEVO: Auto-crear tablas en Supabase ---
+        using (var scope = app.Services.CreateScope())
+        {
+            var db = scope.ServiceProvider.GetRequiredService<ElEspirituDeLaTormentaDbContext>();
+            db.Database.EnsureCreated();
+        }
+        // --- FIN DEL CÓDIGO NUEVO ---
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
-app.UseHttpsRedirection();
+        app.UseHttpsRedirection();
 
-app.UseCors("PermitirVercel");
-app.UseAuthorization();
+        app.UseCors("PermitirVercel");
+        app.UseAuthorization();
 
-app.MapControllers();
+        app.MapControllers();
 
-app.MapFallbackToFile("/index.html");
+        app.MapFallbackToFile("/index.html");
 
-app.Run();
+        app.Run();
