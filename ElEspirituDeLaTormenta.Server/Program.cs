89c569fbf80fd6dfd9e8629d6351a4880e3afddf
Gustavo Narancio <eze_tavo@hotmail.com>
2026-04-08 23:55:40 -0300

    nuevos cambios

--- ElEspirituDeLaTormenta.Server/Program.cs
+++ ElEspirituDeLaTormenta.Server/Program.cs
@@ -1,6 +1,10 @@
 using ElEspirituDeLaTormenta.Server.Models;
 using Microsoft.EntityFrameworkCore;
 
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +16,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Configuración de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirVercel",
+        policy =>
+        {
+            // Aca pones la URL exacta que te dio Vercel (sin la barra / al final)
+            // Tambien dejamos el localhost por si queres seguir probando en tu PC
+            policy.WithOrigins("http://localhost:4200", "https://tu-juego-real.vercel.app")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -27,6 +44,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("PermitirVercel");
 app.UseAuthorization();
 
 app.MapControllers();
